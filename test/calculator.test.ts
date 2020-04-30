750228b87b563c53392c5d3a4f7de020b3b1bb38
Gianluca Moretti <gianluca.moretti@jobrapido.com>
2020-04-30 03:17:37 +0200

    Added unit tests

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -45,4 +45,39 @@ describe("Calculator instance", () => {
       Times.once()
     );
   });
+
+  it("given multiple decimals should calculate the exchange rate applying the right approximation", async () => {
+    exchangeRateServiceMock
+      .setup((rateService) =>
+        rateService.getRate(It.isValue("EUR"), It.isAny())
+      )
+      .returns(() => Promise.resolve(1));
+
+    exchangeRateServiceMock
+      .setup((rateService) => rateService.getRate("USD", It.isAny()))
+      .returns(() => Promise.resolve(1.085343898423984));
+
+    const amount: number = 1;
+    const srcCurrency: string = "EUR";
+    const destCurrency: string = "USD";
+    const refDate: Date = new Date("2020/04/28");
+
+    const actual = await sut.calculate(
+      amount,
+      srcCurrency,
+      destCurrency,
+      refDate
+    );
+    expect(actual).toEqual(1.0853);
+
+    exchangeRateServiceMock.verify(
+      (rateService) => rateService.getRate("EUR", refDate),
+      Times.once()
+    );
+
+    exchangeRateServiceMock.verify(
+      (rateService) => rateService.getRate("USD", refDate),
+      Times.once()
+    );
+  });
 });
